ef9aaaedc3e75fdd7093605e1660ca6f6d937062
Ajay <ajaychandru20@gmail.com>
2024-05-17 15:28:54 +0530

    covered CSV readfile using diff concepts

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("cypress");
-const csv = require('csv-parser');
+const csvFs = require('csv-parser');
 const fs = require('fs');
+const csv = require('@fast-csv/parse');
+// const readXlsx = require("./cypress/plugins/read-xlsx");
 
 module.exports = defineConfig({
   projectId: "k2mbs8",
@@ -8,23 +10,48 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config, browser) {
       on('task', {
-        readCSVFile(filePath, jsonFilePath) {
+        readCSVFile({ filePath, jsonFilePath, headers }) {
           return new Promise((resolve, reject) => {
             const jsonArray = [];
+            // Create a read stream for the CSV file
             fs.createReadStream(filePath)
-              .pipe(csv())
-              .on('data', (data) => jsonArray.push(data))
+              .pipe(csvFs({headers}))
+              .on('data', (data) => {
+                jsonArray.push(data); // Push each row into the jsonArray
+              })
               .on('end', () => {
+                // Write the JSON array to the specified file path
                 fs.writeFile(jsonFilePath, JSON.stringify(jsonArray, null, 2), (err) => {
                   if (err) {
-                    reject(err);
+                    reject(err); // Reject the promise if there's an error writing the file
                   } else {
-                    resolve();
+                    resolve(null); // Resolve the promise if writing is successful
                   }
                 });
+              })
+              .on('error', (err) => {
+                reject(err); // Reject the promise if there's an error reading the file
               });
           });
         },
+      });
+      //akash approch
+      // on("task", {
+      //   readXlsx: readXlsx.read,
+      // });
+      on("task", {
+        readFromCsv({ path, headers }) {
+          return new Promise(resolve => {
+            let dataArray = [];
+            csv.parseFile(`cypress/${path}`, { headers })
+              .on('data', (data) => {
+                dataArray.push(data);
+              })
+              .on('end', () => {
+                resolve(dataArray);
+              });
+          });
+        }
       });
     },
   },
--- cypress/e2e/readCSV.cy.js
+++ cypress/e2e/readCSV.cy.js
@@ -6,56 +6,57 @@ const fs = require('fs');
 describe("Read the CSV file", () => {
     let tableData;
 
-    before("get the table data", () => {
+    it("using the stringify neat-csv format", () => {
+
         cy.fixture("intermediary.csv")
             .then(neatCSV)
             .then(data => {
                 tableData = data;
                 console.table(tableData);
-                const jsonData = JSON.stringify(data);
-                cy.writeFile('cypress/fixtures/reportResponse.json', data);
+                const jsonData = JSON.stringify(data, null, 2);
+                cy.writeFile('cypress/fixtures/neatCsvRes.json', jsonData);
             });
-
-    });
-
-
-
-    it("using the stringify neat-csv format",()=>{
-
-        cy.readFile("cypress/fixtures/reportResponse.json").then((resData)=>{
+        cy.readFile("cypress/fixtures/neatCsvRes.json").then((resData) => {
             cy.log(resData[0][0])
         })
     })
 
-    it.skip("simple parser",()=>{
-        cy.fixture("intermediary.csv").then((data) => {
-            parse(data, {bom: true,delimiter: ',' }, (err, output) => {
-              if (err) {
-                console.error(err);
-                return;
-              }
-              const jsonData = JSON.stringify(output);
-              cy.writeFile('cypress/fixtures/reportResponseTwo.json', jsonData);
-            });
-          });
-    });
-
-
-
-    it.skip("using parser in task commend", () => {
-        cy.task('readCSVFile', { filePath: "cypress/fixtures/intermediary.csv", jsonFilePath: "cypress/fixtures/reportResponse2.json" })
+    it("using csv-parser and fs approch in task command", () => {
+        cy.task('readCSVFile', { filePath: "cypress/fixtures/intermediary.csv", jsonFilePath: "cypress/fixtures/csv_parserUsingFS.json", headers: ['emp heading', 'name'] })
             .then(() => {
-                cy.readFile("cypress/fixtures/reportResponse2.json").then((testData) => {
+                cy.readFile("cypress/fixtures/csv_parserUsingFS.json").then((testData) => {
+                    console.table(testData)
                     var firstRow = testData[4]; // Assuming you want to use the 5th row (index 4) from the JSON file
                     cy.log(firstRow);
                     // Now you can use the data from the JSON file in your test
                     // For example, you can use it to perform assertions or interact with your application
                 });
             })
-            .catch((err) => {
-                // Handle any errors that occurred during the reading or storing process
-                throw err;
-            });
     });
-    
+
+    it("using akash, csv-parser and fast-csv in task command ",()=>{
+
+        cy.task('readFromCsv',{path:'fixtures/intermediary.csv', headers: false}).then((returnData)=>{
+            cy.writeFile('cypress/fixtures/csv_parserAkashApproch.json',returnData);
+            cy.log(returnData[0][0])
+            console.table(returnData)
+        });
+
+    })
+
+
+    it.skip("simple parser", () => {
+        cy.fixture("intermediary.csv").then((data) => {
+            parse(data, { bom: true, delimiter: ',' }, (err, output) => {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+                const jsonData = JSON.stringify(output);
+                cy.writeFile('cypress/fixtures/reportResponseTwo.json', jsonData);
+            });
+        });
+    });
+
+
 });
